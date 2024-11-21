255d550e89cadfef41cab1574302e10775a31469
박진영 <am030813@naver.com>
2024-11-21 17:19:59 +0900

    FEAT: 사용자의 나이를 기준으로 심박수, 기온, 체온 기준값을 판단해 몇 가지 값을 초과했는지 측정

--- src/main/java/iot/util/UserUtil.java
+++ src/main/java/iot/util/UserUtil.java
@@ -1,27 +1,73 @@
 package iot.util;
 
+import iot.dto.SensorDto;
 import iot.repository.UserRepository;
 import iot.domain.User;
 
 import lombok.AllArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
+import static iot.util.StatusStandard.*;
+
 @Component
-@AllArgsConstructor
 public class UserUtil {
     @Autowired
     private UserRepository userRepository;
 
+    public StatusStandard statusStandard;
+
+    public UserUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     public User findUser(String name){
         return userRepository.findUserByName(name);
     }
-    public void switchStatus(User user){
-        String userName = user.getName();
-        int currentStatus = userRepository.findStatusByName(userName);
-        user.setStatus((currentStatus + 1) % 3);
+    public void switchStatus(User user, int overNum){
+        user.setStatus(overNum);
         userRepository.save(user);
+    }
+    public StatusStandard judgeAge(User user){
+        int age = user.getAge();
+        if(age >=10 && age<20){
+            statusStandard = TEENAGER;
+        }
+        else if(age>=20 && age <65){
+            statusStandard = ADULT;
+        }
+        else if(age >= 65){
+            statusStandard = SENIOR;
+        }
+        return statusStandard;
+    }
+    public int calculateValue(SensorDto sensorDto, StatusStandard statusStandard) {
+        int isOver = 0;
+        LocalDate ld = LocalDate.now();
+        int month = ld.getMonthValue();
+        if (month >= 6 && month <= 9) { // 온열질환
+            if (sensorDto.getAirTemp() >= statusStandard.getSummerAir()) {
+                isOver += 1;
+            }
+            if (sensorDto.getBodyTemp() >= statusStandard.getBodyMax()) {
+                isOver += 1;
+            }
+            if (sensorDto.getPulse() >= statusStandard.getBpmMax()) {
+                isOver += 1;
+            }
+        } else if (month >= 11 || month <= 3) { // 한랭질환
+            if (sensorDto.getAirTemp() <= statusStandard.getSummerAir()) {
+                isOver += 1;
+            }
+            if (sensorDto.getBodyTemp() <= statusStandard.getBodyMax()) {
+                isOver += 1;
+            }
+            if (sensorDto.getPulse() <= statusStandard.getBpmMax()) {
+                isOver += 1;
+            }
+        }
+        return isOver;
     }
 }
