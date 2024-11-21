6d0fc928a9e6aabd140251782a723dbe232c9c14
박진영 <am030813@naver.com>
2024-11-21 17:20:21 +0900

    FEAT: 통합 센서 서비스 로직

--- src/main/java/iot/service/SensorService.java
+++ src/main/java/iot/service/SensorService.java
@@ -22,47 +22,7 @@ public class SensorService {
 
     @Transactional
     public void updateStatus(SensorDto sensorDto, User user){
-        int overNum = calculateValue(sensorDto, judgeAge(user));
+        int overNum = userUtil.calculateValue(sensorDto, userUtil.judgeAge(user));
         userUtil.switchStatus(user, overNum);
-    }
-    public StatusStandard judgeAge(User user){
-        int age = user.getAge();
-        if(age >=10 && age<20){
-            statusStandard = TEENAGER;
-        }
-        else if(age>=20 && age <65){
-            statusStandard = ADULT;
-        }
-        else if(age >= 65){
-            statusStandard = SENIOR;
-        }
-        return statusStandard;
-    }
-    public int calculateValue(SensorDto sensorDto, StatusStandard statusStandard) {
-        int isOver = 0;
-        LocalDate ld = LocalDate.now();
-        int month = ld.getMonthValue();
-        if (month >= 6 && month <= 9) { // 온열질환
-            if (sensorDto.getAirTemp() >= statusStandard.getSummerAir()) {
-                isOver += 1;
-            }
-            if (sensorDto.getBodyTemp() >= statusStandard.getBodyMax()) {
-                isOver += 1;
-            }
-            if (sensorDto.getPulse() >= statusStandard.getBpmMax()) {
-                isOver += 1;
-            }
-        } else if (month >= 11 || month <= 3) { // 한랭질환
-            if (sensorDto.getAirTemp() <= statusStandard.getSummerAir()) {
-                isOver += 1;
-            }
-            if (sensorDto.getBodyTemp() <= statusStandard.getBodyMax()) {
-                isOver += 1;
-            }
-            if (sensorDto.getPulse() <= statusStandard.getBpmMax()) {
-                isOver += 1;
-            }
-        }
-        return isOver;
     }
 }
