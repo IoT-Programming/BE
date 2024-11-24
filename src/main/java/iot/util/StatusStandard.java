8954b1a9b8e9da353ac5b6cee88f18c0c292d034
박진영 <am030813@naver.com>
2024-11-25 00:30:01 +0900

    FEAT: SENIOR의 기준점 변경

--- src/main/java/iot/util/StatusStandard.java
+++ src/main/java/iot/util/StatusStandard.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public enum StatusStandard {
     TEENAGER(10,70, 100, 36.1, 37.2),
     ADULT(20, 60, 100, 36.1, 37.2),
-    SENIOR(65, 60, 100, 36.1, 37.2);
+    SENIOR(65, 60, 100, 35.8, 36.9);
 
     int age;
     double bpmMin;
