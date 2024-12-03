e36cbe0f60f05cdf104fdb859e63900460321d65
박진영 <am030813@naver.com>
2024-12-03 15:23:39 +0900

    FEAT: 생산자 ip 주소 변경

--- src/main/java/iot/config/KafkaConfig.java
+++ src/main/java/iot/config/KafkaConfig.java
@@ -46,7 +46,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, SensorDto> producerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.128.0.2:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(props);
